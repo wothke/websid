cbd8e47b07abdd7d55705fc94c2ddfb44b73e768
wothke <tinyrsid@wothke.ch>
2018-01-27 22:40:00 +0100

    fixed make scripts to support WebAssembly output - prerequisite emscripten 1.37.29

--- makeEmscripten.sh
+++ makeEmscripten.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-emcc -s VERBOSE=0 -Wno-pointer-sign -I./src -Os -O3 --closure 1 --llvm-lto 1 -s NO_FILESYSTEM=1 src/memory.c src/base.c src/cpu.c src/hacks.c src/cia.c src/vic.c src/rsidengine.c src/sid.c src/digi.c src/sidplayer.c -s EXPORTED_FUNCTIONS="['_alloc','_loadSidFile', '_playTune', '_getMusicInfo', '_getSampleRate', '_getSoundBuffer', '_getSoundBufferLen', '_computeAudioSamples']" -o htdocs/tinyrsid2.html && cat shell-pre.js htdocs/tinyrsid2.js shell-post.js > htdocs/tinyrsid.js && rm htdocs/tinyrsid2.html && rm htdocs/tinyrsid2.js && cat htdocs/tinyrsid.js tinyrsid_adapter.js > htdocs/backend_tinyrsid.js
-
+set -e
+emcc -s VERBOSE=0 -Wno-pointer-sign -I./src -Os -O3  --memory-init-file 0 --closure 1 --llvm-lto 1 -s NO_FILESYSTEM=1 src/memory.c src/base.c src/cpu.c src/hacks.c src/cia.c src/vic.c src/rsidengine.c src/sid.c src/digi.c src/sidplayer.c -s EXPORTED_FUNCTIONS="['_alloc','_loadSidFile', '_playTune', '_getMusicInfo', '_getSampleRate', '_getSoundBuffer', '_getSoundBufferLen', '_computeAudioSamples', '_enableVoices', '_malloc', '_free']" -o htdocs/tinyrsid2.js -s 'EXTRA_EXPORTED_RUNTIME_METHODS=["ccall"]' -s WASM=0 -s SINGLE_FILE=1 -s BINARYEN_ASYNC_COMPILATION=0 -s 'BINARYEN_TRAP_MODE="clamp"'
+cat shell-pre.js > htdocs/tinyrsid.js
+cat htdocs/tinyrsid2.js >> htdocs/tinyrsid.js
+cat shell-post.js >> htdocs/tinyrsid.js
+cat htdocs/tinyrsid.js > htdocs/backend_tinyrsid.js
+cat tinyrsid_adapter.js >> htdocs/backend_tinyrsid.js
